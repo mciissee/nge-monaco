7b0aa6a23e8d64720f37b06ebe0e69dba5522a41
Mamadou Cisse <mciissee@gmail.com>
2020-08-27 12:25:44 +0200

    build: update postbuild script

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -4,5 +4,6 @@ mkdir dist/nge-monaco/assets/monaco
 cp -rf node_modules/monaco-editor/min dist/nge-monaco/assets/monaco/min
 cp -rf node_modules/monaco-editor/min-maps dist/nge-monaco/assets/monaco
 
+cp -rf ./projects/nge-monaco/src/lib/themes ./dist/nge-monaco/assets/themes
 cp -rf ./README.md ./dist/nge-monaco
 cp -rf ./LICENSE ./dist/nge-monaco
